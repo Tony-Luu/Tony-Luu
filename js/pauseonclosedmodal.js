ec28cfc72a54f5c9d2d623f6d012f93c49a00add
Tony-Luu <tonyluu@hotmail.co.uk>
2023-08-07 23:41:22 +0100

    Update pauseonclosedmodal.js

--- js/pauseonclosedmodal.js
+++ js/pauseonclosedmodal.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-	$('#body').on('hidden.bs.modal', '.modal', function () {
-		$('#footage').trigger('pause');
+	/* Get iframe src attribute value i.e. YouTube video url
+	    and store it in a variable */
+	    var url = $("#footage").attr('src');
+	    
+	    /* Assign empty url value to the iframe src attribute when
+	    modal hide, which stop the video playing */
+	    $("#col-lg-8").on('hide.bs.modal', function(){
+	        $("#footage").attr('src', '');
+	    });
+	    
+	    /* Assign the initially stored url back to the iframe src
+	    attribute when modal is displayed again */
+	    $("#col-lg-8").on('show.bs.modal', function(){
+	        $("#footage").attr('src', url);
+	    });
 	});
 });
