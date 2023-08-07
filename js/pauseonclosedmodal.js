e01880716e7ea663b93e9b50ecd5d2fee754d2e2
Tony-Luu <tonyluu@hotmail.co.uk>
2023-08-07 22:09:13 +0100

    Updated pauseonclosedmodal.js

--- js/pauseonclosedmodal.js
+++ js/pauseonclosedmodal.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
-    $('.modal').on('hidden.bs.modal', function() { //show modal event for an element which has class 'modal'
-		var id = $(this).attr('id');//saves in the var the ID value of the closed modal
-		var video = document.getElementById(id).querySelectorAll("footage");//Find the element 'video' inside of the modal defined by the ID previosly saved
-
-        $(video)[0].pause(); //pauses the video
-        $(video)[0].currentTime = 0; //rests the video to 0 for have it from the beging when the user opens the modal again
+	$('body').on('hidden.bs.modal', '.modal', function () {
+		$('video').trigger('pause');
 	});
 });
