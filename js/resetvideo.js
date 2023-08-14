a1922d8bccd88f52f284af313013a74d05afd99f
Tony-Luu <tonyluu@hotmail.co.uk>
2023-08-14 01:26:03 +0100

    Update resetvideo.js

--- js/resetvideo.js
+++ js/resetvideo.js
@@ -1,8 +1,6 @@
 (function ($) {
   $(document).on('hidden.bs.modal', function (e) {
-    alert(e.target.id);
     var video = document.getElementById('footage');
-    alert(document.getElementById('footage'));
     video.pause();
     video.currentTime = 0;
   });
