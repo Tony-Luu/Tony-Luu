19615a086a2dffe5f3cc90e072eff069b4cb2c52
Tony Luu <tony.luu@twinkl.co.uk>
2023-08-27 04:08:21 +0100

    Updated resetvideo.js

--- js/resetvideo.js
+++ js/resetvideo.js
@@ -4,6 +4,13 @@
     for(var i = 0; i < videoList.length; i++)
     {
       videoList[i].pause();
+    }
+  });
+
+  $(document).on('hidden.bs.modal', function (e) {    
+    var videoList = document.getElementsByTagName("video");
+    for(var i = 0; i < videoList.length; i++)
+    {
       videoList[i].currentTime = 0;
     }
   });
--- js/videopreview.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.footagepreview').forEach(function(vid) {
-  vid.onmouseover = function() {
-    this.play();
-  }
-  vid.onmouseout = function() {
-    this.load(); // stop and show poster
-  }
-})
